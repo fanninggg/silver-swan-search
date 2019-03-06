b62bcb4b884ba24e1d4058a4eb682d1c89abef83
Ben Wright <ben1991wright@hotmail.co.uk>
2019-03-06 13:15:38 +0000

    add experience to devise sanitzer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :gender, :dob, :location, :nationality, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :gender, :dob, :experience, :location, :nationality, :bio])
   end
 end
