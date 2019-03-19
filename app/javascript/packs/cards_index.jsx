8e0c2e3828207a26c4fa2f121ada80b4e5b2302f
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2019-03-19 14:54:28 +0000

    Fixed react display issue and added a navbar

--- app/javascript/packs/cards_index.jsx
+++ app/javascript/packs/cards_index.jsx
@@ -17,25 +17,23 @@ function makeCards(props) {
 function makeTinderCards(props) {
   var cardArray = props.jobsProp.map(function(job) {
     return(
-      <div className="job-index-container">
-        <div className="job-index-tinder-card">
-          <img className="job-index-tinder-photo" src="https://images.pexels.com/photos/722681/white-snow-forest-winter-722681.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-          <div className="job-index-tinder-info">
-            <h1 className="job-index-tinder-title grey roboto bold">{job.title}</h1>
-            <h2 className="job-index-tinder-location roboto regular">Le Manoir, Morzine</h2>
-            <div className="job-index-tinder-salary">
-              <div className="salary-icon"></div>
-              <p className="small-grey-text">£25,000 per annum</p>
-            </div>
-            <div className="job-index-tinder-more-info">
-              <div className="more-info-icon"></div>
-              <p className="small-grey-text underline">More information</p>
-            </div>
+      <div className="job-index-tinder-card" key={job.id}>
+        <img className="job-index-tinder-photo" src="https://images.pexels.com/photos/722681/white-snow-forest-winter-722681.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+        <div className="job-index-tinder-info">
+          <h1 className="job-index-tinder-title grey roboto bold">{job.title}</h1>
+          <h2 className="job-index-tinder-location roboto regular">Le Manoir, Morzine</h2>
+          <div className="job-index-tinder-salary">
+            <div className="salary-icon"></div>
+            <p className="small-grey-text">£25,000 per annum</p>
           </div>
-          <div className="job-index-tinder-buttons">
+          <div className="job-index-tinder-more-info">
+            <div className="more-info-icon"></div>
+            <p className="small-grey-text underline">More information</p>
           </div>
         </div>
-      </div>
+        <div className="job-index-tinder-buttons">
+        </div>
+        </div>
     )
   })
   return cardArray
@@ -44,7 +42,19 @@ function makeTinderCards(props) {
 function makeSmallCards(props) {
   var cardArray = props.jobsProp.map(function(job) {
     return(
-      <h1 className="job-index-tinder-title grey roboto bold">{job.title}</h1>
+      <div className="job-index-list-card-holder" key={job.id}>
+        <div className="job-index-list-card">
+          <img className="job-index-list-photo" src="https://images.pexels.com/photos/722681/white-snow-forest-winter-722681.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+          <div className="job-index-list-info">
+            <h1 className="small-text roboto light-grey no-margin">{job.title}</h1>
+            <h2 className="small-text roboto regular light-grey no-margin">Le Manoir, Morzine</h2>
+            <p className="small-text grey no-margin">£25,000 p/a</p>
+          </div>
+          <div className="job-index-list-more-info">
+            <p className="tiny-text grey underline no-margin">More info</p>
+          </div>
+        </div>
+      </div>
     )
   })
   return cardArray
