9661915696206d4879e12e74340e4143c3d32d7e
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2019-03-19 16:00:08 +0000

    First class component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import "bootstrap";
 import "./test_react";
 import "./home_react";
 import "./silver_swan_button";
-import "./cards_index";
 import '../components/age_slider'
+
+import CardsIndexComponent from "./cards_index";
+
+var cardsIndexElement = document.getElementById('variable-props-test')
+var jobs = JSON.parse(cardsIndexElement.dataset.jobs);
+
+if (cardsIndexElement) {
+    document.addEventListener('DOMContentLoaded', () => {
+      ReactDOM.render(
+        <CardsIndexComponent jobsProp={jobs}/>,
+        cardsIndexElement.appendChild(document.createElement('div')),
+      )
+    })
+  }
