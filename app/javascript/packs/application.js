4660b044800aaf95fb235725fa83597ea92fc8e1
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2019-03-21 11:09:26 +0000

    Cards index with tinder and list cards and switch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import "bootstrap";
 import "./test_react";
-import "./home_react";
 import "./silver_swan_button";
+import "./switch";
 import '../components/age_slider'
+
+// ********************Tinder Cards*******************************************************
+
+import CardIndex from "./cards_index";
+
+var cardsIndexElement = document.getElementById('variable-props-test')
+var jobs = JSON.parse(cardsIndexElement.dataset.jobs);
+
+
+if (cardsIndexElement) {
+  document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <CardIndex />,
+      cardsIndexElement.appendChild(document.createElement('div')),
+    )
+  })
+}
+
+// ********************Switch*******************************************************
+
+// import Switch from "./switch";
+
+// var switchId = document.getElementById('switch')
+
+// if (switchId) {
+//   document.addEventListener('DOMContentLoaded', () => {
+//     ReactDOM.render(
+//       <Switch />,
+//       switchId.appendChild(document.createElement('div')),
+//     )
+//   })
+// }
 
 // ************************Old Cards Index Component*************************************
 
@@ -24,20 +56,3 @@ import '../components/age_slider'
 //     )
 //   })
 // }
-
-// ********************Tinder Cards*******************************************************
-
-import CardList from "./cards_index";
-
-var cardsIndexElement = document.getElementById('variable-props-test')
-var jobs = JSON.parse(cardsIndexElement.dataset.jobs);
-
-
-if (cardsIndexElement) {
-  document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(
-      <CardList />,
-      cardsIndexElement.appendChild(document.createElement('div')),
-    )
-  })
-}
