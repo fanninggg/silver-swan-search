7c57bee05aed3b8f91d25bea8da93a3c5073be38
Ben Wright <ben1991wright@hotmail.co.uk>
2019-03-26 13:23:47 +0000

    add backend to search users by jobs they have applied for

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_161036) do
+ActiveRecord::Schema.define(version: 2019_03_26_132204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_03_25_161036) do
     t.datetime "updated_at", null: false
     t.string "salary"
     t.string "salary_type"
+    t.string "sector"
   end
 
   create_table "users", force: :cascade do |t|
